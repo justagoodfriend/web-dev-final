e91b98312e2785f5124190c98845edaa7002bfd4
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-05 00:19:49 -0400

    bug fixing

--- src/profile-page-components/settings/settings.js
+++ src/profile-page-components/settings/settings.js
@@ -2,39 +2,45 @@
 
 const Settings = () => {
     return (
-        <form for="profile-fields" className="custom-padding-left pt-3 d-flex flex-row">
+        <div className="custom-padding-left pt-3 d-flex flex-row">
             <div className="d-flex flex-column col-7">
-                <div>
-                    <label className="pb-1" for="email"><b>Email</b></label><br/>
-                    <input className="bg-secondary bg-opacity-10 rounded-3 border border-1 border-primary p-3 py-2 mb-2" id="email" value="Aliyah_9@gmail.com"/>
-                </div>
-                <div>
-                    <label className="pb-1" for="username"><b>Username</b></label><br/>
-                    <input className="bg-secondary bg-opacity-10 rounded-3 border border-1 border-primary p-3 py-2 mb-2" id="username" value="Aliyah_9"/>
-                </div>
-                <div>
-                    <label className="pb-1" for="password"><b>Password</b></label><br/>
-                    <input className="bg-secondary bg-opacity-10 rounded-3 border border-1 border-primary p-3 py-2 mb-2" id="password" value="WebDev2023!"/>
-                </div>
+                <label>
+                    <div className="pb-1">
+                        <b>Email</b>
+                    </div>
+                    <input className="bg-secondary bg-opacity-10 rounded-3 border-2 border-purple p-3 py-2 mb-2 w-75" defaultValue="Aliyah_9@gmail.com"/>
+                </label>
+                <label>
+                    <div className="pb-1">
+                        <b>Username</b>
+                    </div>
+                    <input className="bg-secondary bg-opacity-10 rounded-3 border-2 border-purple p-3 py-2 mb-2 w-75" defaultValue="Aliyah_9"/>
+                </label>
+                <label>
+                    <div className="pb-1">
+                        <b>Password</b>
+                    </div>
+                    <input className="bg-secondary bg-opacity-10 rounded-3 border-2 border-purple p-3 py-2 mb-2 w-75" defaultValue="WebDev2023!"/>
+                </label>
                 <div className="pt-4">
-                    <button className="bg-primary text-white rounded-3 no-border px-4 py-1">
+                    <button className="background-purple text-white rounded-3 no-border px-4 py-1">
                         Save
                     </button>
-                    <button className="no-background no-border text-primary px-4 py-1">
+                    <button className="no-background no-border text-purple px-4 py-1">
                         Cancel
                     </button>
                 </div>
             </div>
-            <div className="col-4 pt-3">
-                <img alt="..." src={`/images/profile-empty.jpeg`} className="rounded-3 profile-pic-larger2"/>
-                <button className="no-border no-background text-center">
-                    Upload new image
+            <div className="col-4 pt-3 text-center">
+                <img alt="..." src={`/images/profile-empty.jpeg`} className="rounded-4 profile-pic-larger2"/>
+                <button className="no-border no-background pt-3 text">
+                    <h5>Upload new image</h5>
                 </button>
-                <input type="file" className="no-border no-background text-center">
+                <button className="no-border no-background">
                     me_photo.jpg
-                </input>
+                </button>
             </div>
-        </form>
+        </div>
     )
 }
 
