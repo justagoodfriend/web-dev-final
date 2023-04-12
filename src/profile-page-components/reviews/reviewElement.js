a0113e720c53d60e6eee60515a21b3eab814bde0
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-12 12:53:06 -0400

    more minor changes

--- src/profile-page-components/reviews/reviewElement.js
+++ src/profile-page-components/reviews/reviewElement.js
@@ -13,10 +13,9 @@ const ReviewElement = ({
         <div className="my-3">
             <h5>{review.title}</h5>
             <div className="rounded-4 bg-secondary bg-opacity-25 p-4 me-5">
-
-                <div className="row">
+                <div className="row d-flex flex-nowrap">
                     <div className="col-2 d-flex align-items-center">
-                        <div className="ms-4">
+                        <div className="ms-3">
                             <img src={`/images/profile-empty.jpeg`} alt="..."
                                  className="profile-pic-round-mini rounded-circle"/>
                             <div className="text-center">
@@ -24,9 +23,13 @@ const ReviewElement = ({
                             </div>
                         </div>
                     </div>
-                    <div className="col-10">
+                    <div className="col-9 pe-0">
                         <Stars key={review._id} rating={review.rating}/>
                         {review.content}
+                    </div>
+                    <div className="col-auto p-0">
+                        <i className="bi bi-pen pe-2"/>
+                        <i className="bi bi-x-lg"/>
                     </div>
                 </div>
             </div>
