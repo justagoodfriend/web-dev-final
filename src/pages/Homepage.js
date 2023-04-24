08a2779180da4025f768cd94ec55bcd2e8f9f883
justagoodfriend <greenate1@gmail.com>
2023-04-24 13:50:03 -0400

    maybe not the greatest solution

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,20 +7,25 @@ import { useSelector } from "react-redux";
 //renders the initial homepage:, somethings up with the CSS styling and I have no idea what it is...
 //for some reason, everythingn is still in block and the other attributes, including the cascade no longer work which is nice.
 const HomePage = () => {
-    const user = useSelector((state) => state.users.currentUser);
-    return (
-        <div className="row">
-            <UserSection active="Home" />
-            <div className="col-9">
-                {console.log(user && user.items)}
-                {(user && user.wishlist) && <CarouselItems title="Recommended" id="R1" />}
-                {(user && user.wishlist) && <CarouselItems title="Wishlist" id="W1" />}
-                {/** FIGURE OUT THE WORKAROUND FOR IDs */}
-                {(!user || !user.username || user.items) && <CarouselItems title="Best Sellers" id="W1"/>}
-                {(!user || !user.username || user.items) && <CarouselItems title="Recent Additions" id="R1"/>}
-                <Outlet />
-            </div>
-        </div>
-    );
+  const user = useSelector((state) => state.users.currentUser);
+  console.log(user);
+  return (
+    <div className="row">
+      <UserSection active="Home" />
+      <div className="col-9">
+        {console.log(user && user.items)}
+        {user && user.wishlist && <CarouselItems title="Recommended" id="R1" />}
+        {user && user.wishlist && <CarouselItems title="Wishlist" id="W1" />}
+        {/** FIGURE OUT THE WORKAROUND FOR IDs */}
+        {(!user || !user.username || user.items) && (
+          <CarouselItems title="Best Sellers" id="W1" />
+        )}
+        {(!user || !user.username || user.items) && (
+          <CarouselItems title="Recent Additions" id="R1" />
+        )}
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 export default HomePage;
