0ce1fd45e89b6d65627de77f2b19cf43fe071581
justagoodfriend <greenate1@gmail.com>
2023-04-23 20:49:54 -0400

    fix bug stopping api page rendering

--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -6,24 +6,21 @@ import SearchMen from "../search-page-components/search-buttons/searchMen";
 import SearchKids from "../search-page-components/search-buttons/searchKids";
 import SearchSale from "../search-page-components/search-buttons/searchSale";
 
-const SearchPage = ({page = "None"}) => {
-    const currentUser = useSelector((state) => state.users.currentUser);
-    useEffect(() => {
-        currentUser();
-    }, []);
-    if (page === "None") {
-        return (<SearchResults/>);
-    } else if (page === "Women") {
-        return (<SearchWomen/>);
-    }
-    else if (page === "Men") {
-        return (<SearchMen/>);
-    }
-    else if (page === "Kids") {
-        return (<SearchKids/>);
-    }
-    else if (page === "Sale") {
-        return (<SearchSale/>);
-    }
-}
+const SearchPage = ({ page = "None" }) => {
+  const currentUser = useSelector((state) => state.users.currentUser);
+  // useEffect(() => {
+  //     currentUser();
+  // }, []);
+  if (page === "None") {
+    return <SearchResults />;
+  } else if (page === "Women") {
+    return <SearchWomen />;
+  } else if (page === "Men") {
+    return <SearchMen />;
+  } else if (page === "Kids") {
+    return <SearchKids />;
+  } else if (page === "Sale") {
+    return <SearchSale />;
+  }
+};
 export default SearchPage;
