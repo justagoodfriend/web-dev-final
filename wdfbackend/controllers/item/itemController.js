0c27e0c546623b982cf2e4004285fb00f3981c8a
justagoodfriend <greenate1@gmail.com>
2023-04-26 15:34:53 -0400

    updated details page to include created items, and api items

--- wdfbackend/controllers/item/itemController.js
+++ wdfbackend/controllers/item/itemController.js
@@ -1,41 +1,42 @@
 import * as dao from "./item-dao.js";
 
 const itemController = (app) => {
-    const getItems = async (req, res) => {
-        console.log("getting all items");
-        const items = await dao.findAllItems();
-        res.json(items);
-    }
+  const getItems = async (req, res) => {
+    console.log("getting all items");
+    const items = await dao.findAllItems();
+    res.json(items);
+  };
 
-    const getItemById = async (req, res) => {
-        console.log("getting item with id:", req.params.iid);
-        const item = await dao.findItemByItemId(req.params.iid);
-        res.json(item);
-    }
+  const getItemById = async (req, res) => {
+    console.log("getting item with id:", req.params.iid);
+    const item = await dao.findItemByItemId(req.params.iid);
+    console.log("made it here!");
+    res.json(item);
+  };
 
-    const createItem = async (req, res) => {
-        console.log("creating item");
-        const newItem = await dao.createItem(req.body);
-        res.json(newItem);
-    }
+  const createItem = async (req, res) => {
+    console.log("creating item");
+    const newItem = await dao.createItem(req.body);
+    res.json(newItem);
+  };
 
-    const updateItem = async (req, res) => {
-        console.log("updating item");
-        const status = await dao.updateItem(req.params.rid, req.body);
-        res.json(status);
-    }
+  const updateItem = async (req, res) => {
+    console.log("updating item");
+    const status = await dao.updateItem(req.params.rid, req.body);
+    res.json(status);
+  };
 
-    const deleteItem = async (req, res) => {
-        console.log("deleting item");
-        const status = await dao.deleteItem(req.params.rid);
-        res.json(status);
-    }
+  const deleteItem = async (req, res) => {
+    console.log("deleting item");
+    const status = await dao.deleteItem(req.params.rid);
+    res.json(status);
+  };
 
-    app.get("/api/details", getItems);
-    app.get("/api/details/:iid", getItemById);
-    app.post("/api/details/create", createItem);
-    app.put("/api/update/:iid", updateItem);
-    app.delete("/api/delete/:iid", deleteItem);
+  app.get("/api/details", getItems);
+  app.get("/api/details/:iid", getItemById);
+  app.post("/api/details/create", createItem);
+  app.put("/api/update/:iid", updateItem);
+  app.delete("/api/delete/:iid", deleteItem);
 };
 
 export default itemController;
