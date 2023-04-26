3863df73a0f64435c40fd1c7c7e15061b764611c
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-26 17:24:58 -0400

    add seller id

--- wdfbackend/controllers/item/item-schema.js
+++ wdfbackend/controllers/item/item-schema.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const itemSchema = mongoose.Schema(
     {
         itemId: { type: Number, required: true, unique: true },
+        sellerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'UserModel',
+            required: true
+        },
         title: String,
         price: String,
         colors: [String],
