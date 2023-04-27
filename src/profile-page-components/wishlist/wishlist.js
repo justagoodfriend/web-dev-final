b16c51a1f892b974da2937d2950c242c14bd0047
Sophia Mishurny-Santini <mishurny-santini.s@northeastern.edu>
2023-04-27 10:19:42 -0400

    fixing profile routing

--- src/profile-page-components/wishlist/wishlist.js
+++ src/profile-page-components/wishlist/wishlist.js
@@ -6,35 +6,40 @@ import React, { useEffect, useContext, useState } from "react";
 import { UserContext } from "../../redux/userContextTest";
 import { getFavoritesByUserId } from "../../ApiClient/services/favorites";
 import {useParams} from "react-router";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {findUserByIdThunk} from "../../ApiClient/thunks/authThunks";
 import WishListItem from "../../components/WishlistProfile";
 
 
-const Wishlist = () => {
+const Wishlist = ({
+    userId = null
+                  }) => {
   //get the currentUser here, based on whether the user is a seler/buyer the queries should be different
+  const dispatch = useDispatch();
   const [database, setDatabase] = useState([]);
-  let userId = useParams().uid;
-  const { user } = useContext(UserContext);
-  let currentUser = JSON.parse(user);
+  let paramUserId = useParams().uid;
+  const userViewed = useSelector((state) => state.users.currentTarget);
+
   if (!userId) {
-    userId = currentUser._id;
+    userId = paramUserId;
   }
-  // console.log(currentUser);
 
   //gets the favorited items by a seller
   const fetchSellersItems = async () => {
     //get all items, that correspond to sellerID
+    console.log("getting seller items for", userId);
     const dbItems = await getItemsBySeller(userId);
     if (dbItems) {
-      setDatabase(dbItems);
+      const item = dbItems.map((item) => item.itemId);
+      console.log(item);
+      setDatabase(item);
     }
   };
 
   //gets the favorited items by a buyer
   //TODO: make this work for both logged in user, and the user from the params:
   const fetchBuyerFavorited = async () => {
-    const dbItems = await getFavoritesByUserId(currentUser._id);
+    const dbItems = await getFavoritesByUserId(userId);
     console.log("this is the items returned from query" + dbItems);
     if (dbItems) {
       const item = dbItems.map((item) => item.itemId);
@@ -43,13 +48,19 @@ const Wishlist = () => {
     }
   };
 
+  useEffect(
+      () => {
+        dispatch(findUserByIdThunk(userId));
+      },[]);
+
   useEffect(() => {
-    if (currentUser && currentUser.items) {
+    if (userViewed && userViewed.items) {
       fetchSellersItems();
+      console.log("db items", database)
     } else {
       fetchBuyerFavorited();
     }
-  }, []);
+  }, [userViewed]);
 
   return (
     <div className="cards result-layout">
